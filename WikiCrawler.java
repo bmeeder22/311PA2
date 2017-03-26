d520c2723d7532a43cfd014be1d798ca272b737a
Benjamin Meeder <bmeeder22@gmail.com>
2017-03-26 10:49:06 -0500

    Finished extract links

--- WikiCrawler.java
+++ WikiCrawler.java
@@ -1,4 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class WikiCrawler {
     private String seedUrl;
@@ -12,14 +15,47 @@ public class WikiCrawler {
     }
 
     public ArrayList<String> extractLinks(String doc) {
-        return null;
+        doc = getFirstParagraph(doc);
+        String[] list = doc.split("\"/wiki/");
+        ArrayList<String> out = new ArrayList<>();
+        list[0] = "\"";
+
+        for(String l: list) {
+            String toAdd = "/wiki/" + l.substring(0, l.indexOf("\""));
+            if(isValidURL(toAdd)) out.add(toAdd);
+        }
+        return out;
+    }
+
+    private String getFirstParagraph(String fileText) {
+        int paragraphIndex = fileText.indexOf("<p>");
+        return fileText.substring(paragraphIndex);
+    }
+
+    private boolean isValidURL(String url) {
+        if(url.length() < 7) return false;
+        else if(!url.substring(0, 6).equals("/wiki/")) return false;
+        else if(url.contains("#") || url.contains(":")) return false;
+        return true;
     }
 
     public void crawl() {
 
     }
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws FileNotFoundException {
+        WikiCrawler crawler = new WikiCrawler("test", 1, "test");
+        System.out.println(crawler.isValidURL("/wiki/XXXX")); //True
+        System.out.println(crawler.isValidURL("/wiki/test")); //True
+        System.out.println(crawler.isValidURL("/wiki/test#test")); //False
+        System.out.println(crawler.isValidURL("/wiki/test:test")); //False
+        System.out.println(crawler.isValidURL("/hello/XXXX")); //False
+        System.out.println(crawler.isValidURL("/test")); //False
+        System.out.println(crawler.isValidURL("/wiki")); //False
+        System.out.println(crawler.isValidURL("/wiki/")); //False
 
+        System.out.println();
+        String content = new Scanner(new File("sample.txt")).useDelimiter("\\Z").next();
+        System.out.println(crawler.extractLinks(content));
     }
 }
