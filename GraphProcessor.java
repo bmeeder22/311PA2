1528ed58ddc766f0eaafcc297df24a7d4c827b22
Benjamin Meeder <bmeeder22@gmail.com>
2017-03-25 21:29:14 -0500

    Added skeleton code.

--- GraphProcessor.java
+++ GraphProcessor.java
@@ -1,6 +1,31 @@
-/**
- * Created by benjaminmeeder on 3/21/17.
- */
+import java.util.ArrayList;
+
 public class GraphProcessor {
-    
+    GraphProcessor(String graphData) {
+
+    }
+
+    public int outDegree(String v) {
+        return 0;
+    }
+
+    public boolean sameComponent(String u, String v) {
+        return true;
+    }
+
+    public ArrayList<String> componentVertices(String v) {
+        return null;
+    }
+
+    public int largestComponent() {
+        return 0;
+    }
+
+    public int numComponents() {
+        return 0;
+    }
+
+    public ArrayList<String> bfsPath(String u, String v) {
+        return null;
+    }
 }
--- WikiCrawler.java
+++ WikiCrawler.java
@@ -1,6 +1,25 @@
-/**
- * Created by benjaminmeeder on 3/21/17.
- */
-public class WikiCrawler {
+import java.util.ArrayList;
 
+public class WikiCrawler {
+    private String seedUrl;
+    private int max;
+    private String fileName;
+
+    WikiCrawler(String seedUrl, int max, String fileName) {
+        this.seedUrl = seedUrl;
+        this.max = max;
+        this.fileName = fileName;
+    }
+
+    public ArrayList<String> extractLinks(String doc) {
+        return null;
+    }
+
+    public void crawl() {
+
+    }
+
+    public static void main(String args[]) {
+
+    }
 }
